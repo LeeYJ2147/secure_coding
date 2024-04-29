ad1306d7bb4970d278c2d6615f85dbd77875def0
Yujun Lee <june040929@gmail.com>
2024-04-29 16:18:37 +0900

    with checking

--- Practice/09/09_1_phone.c
+++ Practice/09/09_1_phone.c
@@ -1,20 +1,74 @@
 #include <stdio.h>
 
+int checking_phone(char *phone)
+{
+    int firstDash, secondDash;
+
+    for (firstDash = 0; firstDash < sizeof(phone) && phone[firstDash] != '-'; firstDash++)
+    {
+    }
+    switch (firstDash)
+    {
+    case 2:
+        puts("1-2");
+        if (phone[0] != '0')
+            return 0;
+        if (phone[1] < '1' || phone[1] > '9')
+            return 0;
+        break;
+    case 3:
+        puts("1-3");
+        if (phone[0] != '0')
+            return 0;
+        if (phone[1] < '1' || phone[1] > '9')
+            return 0;
+        if (phone[2] < '0' || phone[1] > '9')
+            return 0;
+        break;
+    default:
+        puts("1-0");
+        return 0;
+    }
+
+    for (secondDash = firstDash + 1; secondDash < sizeof(phone) && phone[secondDash] != '-'; secondDash++)
+    {
+    }
+    printf("%d %d %d\n", firstDash, secondDash, firstDash-secondDash);
+    switch (secondDash - firstDash)
+    {
+    case 4:
+        puts("2-3");
+        for (int i = 1; i < 4; i++)
+            if (phone[firstDash + i] < '1' || phone[firstDash + i] > '9')
+                return 0;
+        break;
+    case 5:
+        puts("2-4");
+        for (int i = 1; i < 5; i++)
+            if (phone[firstDash + i] < '1' || phone[firstDash + i] > '9')
+                return 0;
+        break;
+    default:
+        puts("2-0");
+        return 0;
+    }
+
+    puts("last");
+    for (int i = 1; i < 5; i++)
+        if (phone[secondDash + i] < '1' || phone[secondDash + i] > '9')
+            return 0;
+    puts("oh yeah");
+    return 1;
+}
+
 int main()
 {
-    int permitted=1, firstDash, secondDash;
+    int firstDash, secondDash;
     char phone[15];
     puts("Input number to check: ");
-    gets_s(phone, sizeof(phone));
-
-    for(firstDash=0; firstDash<sizeof(phone) && phone[firstDash]!='-'; firstDash++) {}
-    switch(firstDash){
-        case 2:
-            if(phone[0] != '0') {permitted=0; return permitted;}
-            else if(phone[1])
-        case 3:
-        default:
-            permitted=0;
-            return permitted;
-    }
+    gets(phone);
+    if(checking_phone(phone)) puts("ok");
+    else puts("not permitted");
+    
+    return 0;
 }
