bb59e8c72a7bff5140eff6e2a7f9bfd56d8a9ed5
Yujun Lee <june040929@gmail.com>
2024-05-02 12:53:00 +0900

    added explain

--- Practice/09/09_2_classNumber.c
+++ Practice/09/09_2_classNumber.c
@@ -7,23 +7,24 @@ int check_range(char target, char start, char end)
 
 int checking_classNumber(char *classNumber)
 {
+    // {[A-Z]|[a-z]}3
     for (int i = 0; i < 3; i++)
         if (check_range(classNumber[i], 'A', 'z'))
             return 0;
-
+    // {1-4}
     if (check_range(classNumber[3], '1', '4'))
         return 0;
-
+    // {0-9}3
     for (int i = 4; i < 7; i++)
         if (check_range(classNumber[i], '0', '9'))
             return 0;
-
+    // {-}
     if (classNumber[7] != '-')
         return 0;
-
+    // {0-1}
     if (check_range(classNumber[8], '0', '1'))
         return 0;
-
+    // {0-9}
     if (check_range(classNumber[9], '0', '9'))
         return 0;
 
